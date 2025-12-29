3dfd9dbea9cc0e4c00e7b1872fc6f1276e838af1
Hari Priya <haripriya816141@gmail.com>
2025-12-29 18:34:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+	const match = s.match(/^\s*([^ ]*)/);
+	return match ? match[1] : "";
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
+
